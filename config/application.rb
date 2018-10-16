33318dc884ce43c06a0ac84c29bfbdbc829c2617
Ghislain Loaec <gloaec@cadoles.com>
2018-10-16 16:05:54 +0200

    Change logo in mail

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module CovoiturageLibreRails5
 
     host = ENV['MAILER_HOST'] || 'localhost:3000'
     config.action_mailer.default_url_options = { host: host }
+    config.action_mailer.asset_host = host
     Rails.application.routes.default_url_options[:host] = host
 
     config.assets.paths << Rails.root.join("vendor", "assets", "images")
